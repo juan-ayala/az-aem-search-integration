6be57d1e9856c29dc07958b388878ac6caf83fba
Juan Ayala <juan.ayala.tech@gmail.com>
2025-02-23 21:31:16 -0600

    Delete page from index when it is unpublished

--- test/aem-page-index-delete-doc.test.js
+++ test/aem-page-index-delete-doc.test.js
@@ -1,30 +1,91 @@
-/* 
-* <license header>
-*/
+const action = require('./../actions/aem-page-index-delete-doc/index')
 
-jest.mock('@adobe/aio-sdk', () => ({
-  Core: {
-    Logger: jest.fn()
-  }
-}))
-const { Core } = require('@adobe/aio-sdk')
-const mockLoggerInstance = { info: jest.fn() }
-Core.Logger.mockReturnValue(mockLoggerInstance)
+jest.mock('@azure/identity', () => ({
+  ClientSecretCredential: jest.fn().mockImplementation(() => { return {}; })
+}));
+const { ClientSecretCredential } = require('@azure/identity');
 
-const action = require('./../actions/aem-page-index-delete-doc/index.js')
+const mockDeleteDocuments = jest.fn();
+jest.mock('@azure/search-documents', () => ({
+  SearchClient: jest.fn().mockImplementation(() => ({
+    deleteDocuments: mockDeleteDocuments
+  }))
+}));
+const { SearchClient } = require('@azure/search-documents');
 
 beforeEach(() => {
-  Core.Logger.mockClear()
-  mockLoggerInstance.info.mockReset()
-})
+  jest.clearAllMocks();
+});
 
-describe('test event action', () => {
-  test('main should be defined', () => {
-    expect(action.main).toBeInstanceOf(Function)
-  })
-  test('should set logger to use LOG_LEVEL param', async () => {
-    await action.main({LOG_LEVEL: 'fakeLevel'})
-    expect(Core.Logger).toHaveBeenCalledWith('main',
-        {level: 'fakeLevel'})
-  })
-})
+describe('az-ai-search-index-delete', () => {
+
+  it('can handle delete error', async () => {
+
+    // arrange
+    mockDeleteDocuments.mockRejectedValue(new Error('uh-oh, spaghetti-o'));
+    // act
+    return action.main({
+      data: {
+        path: '/content/myapp/hello/world'
+      }
+    })
+      .then(result => {
+        // assert
+        expect(mockDeleteDocuments).toHaveBeenCalledTimes(1);
+        expect(mockDeleteDocuments).toHaveBeenCalledWith('id', ['contentmyapphelloworld']);
+        expect(result).toEqual({
+          error: {
+            statusCode: 500,
+            body: {
+              error: 'uh-oh, spaghetti-o'
+            }
+          }
+        });
+      });
+  });
+
+  it('can delete the document', () => {
+
+    // arrange
+    const params = {
+      AZURE_TENANT_ID: 'my-az-tenant',
+      AZURE_CLIENT_ID: 'my-az-client-id',
+      AZURE_CLIENT_SECRET: 'my-az-client-secret',
+      AZURE_SEARCH_ENDPOINT: 'my-search-endpoint',
+      AZURE_SEARCH_INDEX_NAME: 'my-search-index',
+      data: {
+        path: '/content/myapp/hello/world'
+      }
+    };
+    mockDeleteDocuments.mockResolvedValue({
+      results: [{
+        key: 'somekey',
+        succeeded: true
+      }]
+    });
+    // act
+    return action.main(params)
+      .then(result => {
+        // assert
+        expect(ClientSecretCredential).toHaveBeenCalledWith(
+          'my-az-tenant',
+          'my-az-client-id',
+          'my-az-client-secret'
+        );
+        expect(SearchClient).toHaveBeenCalledWith(
+          'my-search-endpoint',
+          'my-search-index',
+          {}
+        );
+        expect(mockDeleteDocuments).toHaveBeenCalledWith('id', ['contentmyapphelloworld']);
+        expect(result).toEqual({
+          statusCode: 200,
+          headers: {},
+          body: [{
+            key: 'somekey',
+            succeeded: true
+          }]
+        });
+      });
+  });
+});
